da30ac3d0a52a42f2cb99682a0949df75780c644
webmantra-code <arijit.webmantratech@gmail.com>
2024-12-17 13:29:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ app.use('/translations', translationRoutes);
 // Start server after DB connection
 const startServer = async () => {
     try {
-        await pool.connect();
-        console.log('Database connected successfully!');
+        //await pool.connect();
+        //console.log('Database connected successfully!');
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
